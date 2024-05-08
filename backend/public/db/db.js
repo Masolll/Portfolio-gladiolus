f00320539f9ed9a5852931c4460d214c04cd76b4
Maksim Umaev <yastey637@gmail.com>
2024-05-09 00:31:01 +0500

    rebuild last commit

--- backend/public/db/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-exports.db = {
-    forms: [
-        { "id": 1, "name": "Maks" },
-        { "id": 2, "name": "Alex" }
-    ]
-};
