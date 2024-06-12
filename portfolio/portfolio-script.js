8a385c8af4624c7b4034a0dfa3d0e7d27a62e5e7
Даниил Ященко <168551774+labrapudel@users.noreply.github.com>
2024-06-12 18:07:40 +0500

    Add files via upload
    соединил все файлы в одну папку, так же исправил многие ошибки

---
+++ portfolio/portfolio-script.js
@@ -0,0 +1,4 @@
+document.getElementById("filter-button").addEventListener("click", function () {
+    var filterBlock = document.querySelector(".users-filter");
+    filterBlock.classList.toggle("filter-none");
+});
