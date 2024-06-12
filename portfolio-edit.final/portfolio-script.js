fb5648946c5b50c50abeadcc6193bd8a4e830e34
Даниил Ященко <168551774+labrapudel@users.noreply.github.com>
2024-06-12 17:56:10 +0500

    Delete portfolio-edit.final directory

--- portfolio-edit.final/portfolio-script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("filter-button").addEventListener("click", function () {
-    var filterBlock = document.querySelector(".users-filter");
-    filterBlock.classList.toggle("filter-none");
-});
